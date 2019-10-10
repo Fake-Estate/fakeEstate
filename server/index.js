7134cc6afba33d9056a25c841245589d15c4ed76
Jake Smith <jacob_smith1223@me.com>
2019-10-10 16:48:33 -0600

    endpoints added

--- server/index.js
+++ server/index.js
@@ -42,7 +42,10 @@ app.get('/api/profile/logout', userCtrl.logout)
 app.get('/api/auth/listing/style', authMidd.authenticateUser, createCtrl.getStyles)
 app.post('/api/listing/create', authMidd.authenticateUser, )
 app.post('/api/auth/listing/create', authMidd.authenticateUser, createCtrl.create )
+app.post('/api/auth/listing/create/type/:id', createCtrl.createType)
 app.get('/api/listings', createCtrl.getListings)
+app.get('/api/auth/listing/type', createCtrl.getType)
+
 
 
 app.listen(SERVER_PORT, () => console.log(chalk.cyan(`Serving on port ${SERVER_PORT} 🚀`)))
