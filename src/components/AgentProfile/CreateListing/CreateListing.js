d1aac5f199509c03d661606a4324ef03bda15a96
Sean Schouten <seanschouten@Seans-MacBook-Pro.local>
2019-10-11 11:39:12 -0600

    Some front end stuff

--- src/components/AgentProfile/CreateListing/CreateListing.js
+++ src/components/AgentProfile/CreateListing/CreateListing.js
@@ -33,10 +33,67 @@ export default class CreateListing extends Component {
                     <input type='text'
                         placeholder='MLS'
                         name='mls'
-                        onchange={this.handleChange}
+                        onChange={this.handleChange}
                         value={this.state.mls}
                         className='input'
                         />
+                    <input type='text'
+                        placeholder='Address'
+                        name='address'
+                        onChange={this.handleChange}
+                        value={this.state.address}
+                        className='input'
+                        />
+                    <input type='text'
+                        placeholder='City'
+                        name='city'
+                        onChange={this.handleChange}
+                        value={this.state.city}
+                        className='input'
+                        />
+                    <input type='text'
+                        placeholder='State'
+                        name='state'
+                        onChange={this.handleChange}
+                        value={this.state.state}
+                        className='input'
+                        />
+                    <input type='text'
+                        placeholder='Square Footage'
+                        name='square_footage'
+                        onChange={this.handleChange}
+                        value={this.state.square_footage}
+                        className='input'
+                        />
+                    <input type='text'
+                        placeholder='Bedrooms'
+                        name='bedrooms'
+                        onChange={this.handleChange}
+                        value={this.state.bedrooms}
+                        className='input'
+                        />
+                    <input type='text'
+                        placeholder='Bathrooms'
+                        name='bathrooms'
+                        onChange={this.handleChange}
+                        value={this.state.bathrooms}
+                        className='input'
+                        />
+                    <input type='text'
+                        placeholder='Price'
+                        name='price'
+                        onChange={this.handleChange}
+                        value={this.state.price}
+                        className='input'
+                        />
+                    <input type='text'
+                        placeholder='Description'
+                        name='description'
+                        onChange={this.handleChange}
+                        value={this.state.description}
+                        className='description input'
+                        />
+                    <button>Continue</button>
                 </div>
             </div>
         )
