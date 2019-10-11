70664ba1c4fe6971479a4e39ecf63c9afd1b7e41
Ali <eight.falling.stars@gmail.com>
2019-10-11 11:34:37 -0600

    DELETE WORKSSSSSSSS

--- server/controllers/create_listing_ctrl.js
+++ server/controllers/create_listing_ctrl.js
@@ -1,5 +1,5 @@
 const getStyles = async (req, res) => {
-    try{
+    try {
         const styles = await req.app.get('db').style.get_style()
         return res.status(200).send(styles)
     } catch (error) {
@@ -12,7 +12,6 @@ const getStyles = async (req, res) => {
 const insertStyle = async (req, res) => {
     try {
         const {id} = req.params
-        console.log(typeof id)
         const {style_id} = req.body
         const styles = await req.app.get('db').style.create_style({style_id, id:+id})
         res.status(200).send(styles)
@@ -22,6 +21,25 @@ const insertStyle = async (req, res) => {
         res.status(500).send(error)
     }
 
+}
+
+const deleteStyle = async (req, res) => {
+    try {
+        console.log('hit', req.params)
+        const listing_id = req.params.id
+        const style_id = req.query.style_id
+        if(listing_id && style_id){
+            await req.app.get('db').style.delete_style({listing_id, style_id})
+            res.sendStatus(200)
+        } else {
+            res.sendStatus(204)
+        }
+        
+    } catch (error) {
+        if(error) throw error
+        console.log('There was an error', error)
+        res.status(500).send(error)
+    }
 }
 
 const getRoomsIncluded = async (req, res) => {
@@ -174,6 +192,10 @@ const getIntFeatures = async (req, res) => {
     } catch (error) {
         if(error) throw error
         console.log('There was an error', error)
+
+    }
+}
+
 const getExtFeatures = async(req,res) => {
     try{
         const db = await req.app.get('db')
@@ -266,6 +288,7 @@ const insertOtherFeatures = async (req, res) => {
 module.exports = {
     getStyles,
     insertStyle,
+    deleteStyle,
     create,
     getListings,
     getListingById,
