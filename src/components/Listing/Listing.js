65e19cb94e15006cb243a5e8bfed43efa38a0e3f
Jake Smith <jacob_smith1223@me.com>
2019-10-17 15:57:19 -0600

    listing style

--- src/components/Listing/Listing.js
+++ src/components/Listing/Listing.js
@@ -19,11 +19,36 @@ export default class Listing extends Component {
     }
     render() {
         return (
-            <div>
-                <div className='listing-margin' />
-                <p>{this.state.listing.address}</p>
-                <p>{this.state.listing.city}</p>
-                <p>{this.state.listing.state}</p>
+            <div className="listing-id-main">
+                <div className ="listing-id-container">
+                    <div className='listing-id-img'>
+                        <img src = {this.state.listing.img} className='house'/>
+                    </div>
+                    <div className="listing-id-flex">
+                        <p className='listing-id-address'>{this.state.listing.address}</p>
+                        <p className='listing-id-city'>{this.state.listing.city},</p>
+                    </div>
+                    <div className='listing-id-info'>
+                        <p className='listing-id-state'>{this.state.listing.state}</p>
+                        <p className='listing-id-zip'>{this.state.listing.zip}</p>
+                        <p>MLS: #{this.state.listing.mls}</p>
+                    </div>
+                    <div className='listing-id-rooms'>
+                        <p>{this.state.listing.bedrooms}beds,</p>
+                        <p>{this.state.listing.bathrooms} baths</p>
+                    </div>
+                    <div>
+                        <p className='listing-id-money'>$ {this.state.listing.price}</p>
+                    </div>
+                    <div>
+                        <p>{}</p>
+                    </div>
+                    <div>
+                        <h1 className='description'>Description:</h1>
+                        <p className='listing-id-description'>{this.state.listing.description}</p>
+                    </div>
+                    
+                </div>
             </div>
         )
     }
